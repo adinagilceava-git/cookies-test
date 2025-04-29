51b3a8af1a3de3b90a9fb091614571152c30931a
adinagilceava-git <gilceava.ma.co@student.utcluj.ro>
2025-04-29 18:22:44 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -11,3 +11,20 @@ function clearOutputCookies() {
   const output = document.getElementById("cookies");
   output.textContent = "";
 }
+document.cookie = "test1=Hello; SameSite=None; Secure";
+document.cookie = "test2=World; SameSite=None; Secure";
+
+const cookieValue = document.cookie
+  .split("; ")
+  .find((row) => row.startsWith("test2="))
+  ?.split("=")[1];
+
+function showCookieValue() {
+  const output = document.getElementById("cookie-value");
+  output.textContent = `> ${cookieValue}`;
+}
+
+function clearOutputCookieValue() {
+  const output = document.getElementById("cookie-value");
+  output.textContent = "";
+}
