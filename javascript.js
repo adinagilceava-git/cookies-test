e42980f030e11b7674f9dac9609bdf8577ed24ed
adinagilceava-git <gilceava.ma.co@student.utcluj.ro>
2025-04-29 18:16:26 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-allCookies = document.cookie;
+document.cookie = "name=Oeschger; SameSite=None; Secure";
+document.cookie = "favorite_food=tripe; SameSite=None; Secure";
+
+function showCookies() {
+  const output = document.getElementById("cookies");
+  output.textContent = `> ${document.cookie}`;
+}
+
+function clearOutputCookies() {
+  const output = document.getElementById("cookies");
+  output.textContent = "";
+}
